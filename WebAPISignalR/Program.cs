9626991aa6d047ea6cd2c3369cf24bb0db27dbfa
Hanmant Rachmale <hanmant.rachmale@orikan.com>
2024-12-27 23:49:10 +1100

    Initial commit with existing project code

---
+++ WebAPISignalR/Program.cs
@@ -0,0 +1,49 @@
+
+namespace WebAPISignalR
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("MyPolicy", policy =>
+                    policy.WithOrigins("http://localhost:4200")
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+
+            builder.Services.AddSignalR();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseCors("MyPolicy");
+
+            app.UseHttpsRedirection();
+            app.MapHub<ShoppingListHub>(pattern: "/ShoppingListHub");
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ WebAPISignalR/ShoppingListHub.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Concurrent;
+
+namespace WebAPISignalR
+{
+    public class ShoppingListHub : Hub
+    {
+        private static readonly ConcurrentDictionary<string, List<string>> _groupIdShoppingLists = new();
+
+        public async Task AddItem(string shoppingListId, string itemName)
+        {
+            if (_groupIdShoppingLists.TryGetValue(shoppingListId, out var shoppingList)) { 
+                shoppingList.Add(itemName);
+                await Clients.Group(shoppingListId).SendAsync("ReceiveShoppingList", shoppingList);
+            }
+
+        }
+
+        public async Task RemoveItem(string shoppingListId, string itemName)
+        {
+            if (_groupIdShoppingLists.TryGetValue(shoppingListId, out var shoppingList))
+            {
+                shoppingList.Remove(itemName);
+                await Clients.Group(shoppingListId).SendAsync("ReceiveShoppingList", shoppingList);
+            }
+        }
+
+        public async Task JoinShoppingList(string shoppingListId)
+        {
+            if(_groupIdShoppingLists.TryGetValue(shoppingListId, out var shoppingList))
+            {
+                await Groups.AddToGroupAsync(Context.ConnectionId, shoppingListId);
+                await Clients.Caller.SendAsync("JoinShoppingList", shoppingListId, shoppingList);
+            }
+        }
+
+        public async Task CreateShoppingList()
+        {
+            string shoppingListId = Guid.NewGuid().ToString();
+            _groupIdShoppingLists.TryAdd(shoppingListId, []);
+            await Groups.AddToGroupAsync(Context.ConnectionId, shoppingListId);
+            await Clients.Caller.SendAsync("ShoppingListCreated", shoppingListId);
+        }
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.Others.SendAsync(method: "Method1", $"{Context.ConnectionId} has joined");
+            await Clients.All.SendAsync(method: "Method1", $"{Context.ConnectionId} has joined");
+            await Clients.Caller.SendAsync(method: "Method1", $"{Context.ConnectionId} has joined");
+            await Clients.Groups("XYZ").SendAsync(method: "Method1", $"{Context.ConnectionId} has joined");
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
